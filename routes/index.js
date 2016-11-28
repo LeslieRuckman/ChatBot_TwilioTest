954216f7e688faffd3e32b0acaaa550eddabae70
LeslieRuckman <leslieruckman@gmail.com>
2016-11-28 17:04:48 -0500

    moved things around

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+
+// Twilio
 var twilio = require('twilio');
+
+// Geocoder - we may not need this, but it could be cool?
 var geocoder = require('geocoder');
+
+// Tracery
 var tracery = require('tracery-grammar');
 
 // our db model
 var Status = require("../models/status.js");
-var Meal = require("../models/meal.js");
 
 /**
  * GET '/'
@@ -15,181 +20,145 @@ var Meal = require("../models/meal.js");
  * @param  {Object} req
  * @return {Object} json
  */
- router.get('/', function(req, res) {
+router.get('/', function(req, res) {
 
-   var jsonData = {
-   	'name': 'node-express-api-boilerplate',
-   	'api-status':'OK',
-     'instructions': "Text (646) 762-0870",
-     'format': 'Tell me what\'s on your mind'
-   }
+    var jsonData = {
+        'name': 'node-express-api-boilerplate',
+        'api-status': 'OK',
+        'instructions': "Send a text to (646) 762-0870",
+        'format': 'I\'m your alternate, mirror self, that lives on the other side of the screen. Send me a text to say hello!'
+    }
 
-// respond with json data
-   res.json(jsonData)
- });
+    // respond with json data
+    res.json(jsonData)
+});
 
 // simple route to show an HTML page
-router.get('/sample-page', function(req,res){
-  res.render('sample.html')
-})
+// router.get('/sample-page', function(req,res){
+//   res.render('sample.html')
+// })
 
-// /**
-//  * POST '/api/create'
-//  * Receives a POST request of the new animal, saves to db, responds back
-//  * @param  {Object} req. An object containing the different attributes of the Animal
-//  * @return {Object} JSON
-//  */
-router.post('/twilio-callback', function(req,res){
+router.post('/twilio-callback', function(req, res) {
 
-  // there's lots contained in the body
-  console.log(req.body);
+    // there's lots contained in the body
+    console.log(req.body);
 
-  // the actual message is contained in req.body.Body
-  var incomingMsg = req.body.Body;
-  // full fields available in the req.body:
-// { ToCountry: 'US',
-// ToState: 'NY',
-// SmsMessageSid: 'SM9115146d8b3d53529e6b83a79448a6a9',
-// NumMedia: '0',
-// ToCity: '',
-// SmsSid: 'SM9115146d8b3d53529e6b83a79448a6a9',
-// FromState: 'GA',
-// FromZip: '30294',
-// SmsStatus: 'received',
-// To: '+16468468769',
-// FromCity: 'ATLANTA',
-// ApiVersion: '2010-04-01'
-// NumSegments: '1',
-// MessageSid: 'SM9115146d8b3d53529e6b83a79448a6a9',
-// From: '+14043230470',
-// AccountSid: 'AC7cc044438cf51cbf44b75a095b40bf05',
-// ToZip: '',
-// Body: 'Just testing this demo!',
-// FromCountry: 'US'}
+    // the actual message is contained in req.body.Body
+    var incomingMsg = req.body.Body;
+    // use terminal command - heroku logs --tail
+    // to view the full list of attributes available
+    console.log(incomingMsg);
+    var incomingNum = req.body.From;
 
-  console.log(incomingMsg);
-  var incomingNum = req.body.From;
-
-  // now, let's save it to our Database
-  var msgToSave = {
-    status: incomingMsg,
-    from: incomingNum
-  }
-
-  var tokens = incomingMsg.split(/\W+/);
-  for (var i = 0; i < tokens.length; i++) {
-    var word = tokens[i];
-    console.log(word);
-
-    // Let's create a some interactions using the node tracery grammar module.
-    var grammar = tracery.createGrammar({
-      'animal': ['panda','fox','capybara','iguana'],
-      'emotion': ['sad','happy','angry','jealous'],
-      'origin':['I am #emotion.a# #animal#.'],
-    });
-
-    var example = grammar.addModifiers(tracery.baseEngModifiers);
-
-    var greeting = 'Hi'||'hi'||'hey'||'Hey';
-    var myself = 'my'||'My'||'me'||'Me'||'I'||'am';
-
-    if(word === 'hello'||'Hello'){
-      var response = "Hi there! This is yourself from the otherside of your black mirror, ask me anything.";
-    } else if(word === myself){
-      var response = example;
-    } else{
-      var response = "Interesting... Tell me more.";
+    // now, let's save it to our Database
+    var msgToSave = {
+        status: incomingMsg,
+        from: incomingNum
     }
-    var result = response;
-  }
-
-  var status = new Status(msgToSave)
-
-  status.save(function(err,data){
-    // set up the twilio response
+    var status = new Status(msgToSave)
     var twilioResp = new twilio.TwimlResponse();
-    if(err){
-      // respond to user
-      twilioResp.sms('Oops! We couldn\'t save status --> ' + incomingMsg);
-      // respond to twilio
-      res.set('Content-Type', 'text/xml');
-      res.send(twilioResp.toString());
+
+// Now let's craft our response!
+
+    var tokens = incomingMsg.split(/\W+/);
+    for (var i = 0; i < tokens.length; i++) {
+        var word = tokens[i];
+        console.log(word);
+
+        var greeting = 'Hi' || 'hi' || 'hey' || 'Hey';
+        var myself = 'my' || 'My' || 'me' || 'Me' || 'I' || 'am';
+
+        if (word === 'hello' || 'Hello') {
+            var response = "Hi there! This is yourself from the otherside of your black mirror, ask me anything.";
+        } else if (word === myself) {
+            var response = example;
+        } else {
+            var response = "Interesting... Tell me more.";
+        }
+        twilioResp.sms = response;
     }
-    else {
-      // respond to user
-      twilioResp.sms(result);
-      // respond to twilio
-      res.set('Content-Type', 'text/xml');
-      res.send(twilioResp.toString());
-    }
-  })
+
+
+    // status.save(function(err, data) {
+    //     // set up the twilio response
+    //     if (err) {
+    //         // respond to user
+    //         twilioResp.sms('Oops! We couldn\'t save status --> ' + incomingMsg);
+    //         // respond to twilio
+    //         res.set('Content-Type', 'text/xml');
+    //         res.send(twilioResp.toString());
+    //     } else {
+    //         // respond to user
+    //         twilioResp.sms(result);
+    //         // respond to twilio
+    //         res.set('Content-Type', 'text/xml');
+    //         res.send(twilioResp.toString());
+    //     }
+    // })
 
 
 
 
 })
 
-router.get('/api/get',function(req,res){
+router.get('/api/get', function(req, res) {
 
-  Status.find(function(err,data){
-    if(err){
-      var error = {
-        status: "ERROR",
-        message: err
-      }
-      res.json(error);
-    }
-    else {
-      var jsonData = {
-        status: "OK",
-        statuses: data
-      }
-      res.json(jsonData);
-    }
-  })
-
-})
-
-router.get('/api/get/latest',function(req,res){
-
-  Status.find().sort('-dateAdded').exec(function(err,data){
-    if(err){
-      var error = {
-        status: "ERROR",
-        message: err
-      }
-      res.json(error);
-    }
-    else {
-      var jsonData = {
-        status: "OK",
-        status: data[0]
-      }
-      res.send(data[0].status);
-    }
-  })
+    Status.find(function(err, data) {
+        if (err) {
+            var error = {
+                status: "ERROR",
+                message: err
+            }
+            res.json(error);
+        } else {
+            var jsonData = {
+                status: "OK",
+                statuses: data
+            }
+            res.json(jsonData);
+        }
+    })
 
 })
 
+router.get('/api/get/latest', function(req, res) {
 
-router.get('/api/get/meals',function(req,res){
+    Status.find().sort('-dateAdded').exec(function(err, data) {
+        if (err) {
+            var error = {
+                status: "ERROR",
+                message: err
+            }
+            res.json(error);
+        } else {
+            var jsonData = {
+                status: "OK",
+                status: data[0]
+            }
+            res.send(data[0].status);
+        }
+    })
 
-  Meal.find(function(err,data){
-    if(err){
-      var error = {
-        status: "ERROR",
-        message: err
-      }
-      res.json(error);
-    }
-    else {
-      var jsonData = {
-        status: "OK",
-        meals: data
-      }
-      res.json(jsonData);
-    }
-  })
+})
+
+
+router.get('/api/get/meals', function(req, res) {
+
+    Meal.find(function(err, data) {
+        if (err) {
+            var error = {
+                status: "ERROR",
+                message: err
+            }
+            res.json(error);
+        } else {
+            var jsonData = {
+                status: "OK",
+                meals: data
+            }
+            res.json(jsonData);
+        }
+    })
 
 })
 
